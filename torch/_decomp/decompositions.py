93fb5a3259af59b58c2af096ab9feb4d99355b57
Isuru Fernando <ifernando@quansight.com>
2023-12-05 15:56:07 +0000

    Add decompositions for replication_pad
    ghstack-source-id: 4f3b3374700c93a62f217adcdb5d53c7acf09c7a
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/115113

--- torch/_decomp/decompositions.py
+++ torch/_decomp/decompositions.py
@@ -4025,6 +4025,36 @@ def upsample_bicubic2d_vec(
 @register_decomposition(aten.reflection_pad3d.default)
 @pw_cast_for_opmath
 def _reflection_pad(a: Tensor, padding: Tuple[int, ...]) -> Tensor:
+    return _reflection_or_replication_pad(
+        a,
+        padding,
+        lambda pad: torch.arange(pad, 0, step=-1, device=a.device),
+        lambda pad, length: torch.arange(
+            length - 2, length - 2 - pad, step=-1, device=a.device
+        ),
+    )
+
+
+@register_decomposition(aten.replication_pad1d.default)
+@register_decomposition(aten.replication_pad2d.default)
+@register_decomposition(aten.replication_pad3d.default)
+@pw_cast_for_opmath
+def _replication_pad(a: Tensor, padding: Tuple[int, ...]) -> Tensor:
+    return _reflection_or_replication_pad(
+        a,
+        padding,
+        lambda pad: torch.zeros(pad, device=a.device, dtype=torch.int64),
+        lambda pad, length: torch.ones(pad, device=a.device, dtype=torch.int64)
+        * (length - 1),
+    )
+
+
+def _reflection_or_replication_pad(
+    a: Tensor,
+    padding: Tuple[int, ...],
+    left_fn: Callable[[int], Tensor],
+    right_fn: Callable[[int, int], Tensor],
+) -> Tensor:
     dim = len(padding) // 2
     torch._check(
         a.dim() in (dim + 1, dim + 2),
@@ -4043,19 +4073,15 @@ def _reflection_pad(a: Tensor, padding: Tuple[int, ...]) -> Tensor:
             middle = middle[-padding_left[i] :]
             to_cat = [middle]
         else:
-            left = torch.arange(padding_left[i], 0, step=-1, device=a.device)
+            left = left_fn(padding_left[i])
             to_cat = [left, middle]
 
         if padding_right[i] < 0:
             middle = middle[: -padding_right[i]]
         else:
-            right = torch.arange(
-                inp_shape[i] - 2,
-                inp_shape[i] - 2 - padding_right[i],
-                step=-1,
-                device=a.device,
-            )
+            right = right_fn(padding_right[i], inp_shape[i])
             to_cat.append(right)
+
         idx: List[Any] = [slice(s) for s in result.shape]
         idx[i + nc_dim] = torch.cat(tuple(to_cat))
         result = result[idx]
